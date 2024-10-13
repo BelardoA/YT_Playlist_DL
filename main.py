7296be84d77cf39eb4ffa8affedc58152b2324b5
BelardoA <livintodie4e@gmail.com>
2024-10-13 19:05:17 -0400

    added more logging

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from Modules.main_screen import start_window
+from Modules.logger import create_logger
 
 if __name__ == "__main__":
+    logger = create_logger()
+    logger.info("Starting application...")
     start_window()
